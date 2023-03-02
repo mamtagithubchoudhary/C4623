524d76ef94071bee55ba8394e127310b2dae1b9a
mamtagithubchoudhary <110805152+mamtagithubchoudhary@users.noreply.github.com>
2023-03-02 17:35:25 +0530

    Add files via upload

---
+++ AbstractionTest.java
@@ -0,0 +1,10 @@
+package corejava1;
+
+public class AbstractionTest {
+
+	public static void main(String[] args) {
+		AbstractionData ad= new AbstractionData(1,"mamta","Aurangabad",15000.0);	
+        ad.mailcheck();
+	}
+
+}
