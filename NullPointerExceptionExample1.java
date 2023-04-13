5318c9ae5f5b7245b4209bef39228c847876c233
mamtagithubchoudhary <110805152+mamtagithubchoudhary@users.noreply.github.com>
2023-04-13 17:29:57 +0530

    Add files via upload

---
+++ NullPointerExceptionExample1.java
@@ -0,0 +1,17 @@
+package corejava1;
+
+import java.util.Scanner;
+
+public class NullPointerExceptionExample1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        String input = scanner.nextLine();
+
+        // Attempt to convert input to an integer
+        int number = Integer.parseInt(input); // This line can throw a NullPointerException
+
+        System.out.println("Number: " + number);
+    }
+}
